6fc6668ad834de53f1eb89ef6595e800c0e3ea6f
Artem Varaksa <artvaraksa@gmail.com>
2021-04-18 20:45:28 +0300

    Don't clean before building; SW config tweaks.

--- workbox-config.js
+++ workbox-config.js
@@ -1,12 +1,13 @@
 module.exports = {
 	globDirectory: 'dist/',
 	globPatterns: [
-		'**/*.{css,js,html}'
+		'**/*.{css,js,html,eot,woff2}'
 	],
 	ignoreURLParametersMatching: [
 		/^utm_/,
 		/^fbclid$/
 	],
 	swDest: 'dist/sw.js',
-	dontCacheBustURLsMatching: /.*\.[a-z0-9]{8}\..*/
+	dontCacheBustURLsMatching: /.*\.[a-z0-9]{8}\..*/,
+	cleanupOutdatedCaches: true,
 };
